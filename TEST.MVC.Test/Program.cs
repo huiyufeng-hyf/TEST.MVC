de94139184751549a9e4c5722b12e2122c2543f1
Michael <Michael@HYF>
2018-05-12 14:44:55 +0800

    Added entity model project initially.

---
+++ TEST.MVC.Test/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TEST.MVC.Model;
+
+namespace TEST.MVC.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DbContext dbCxt = new TestMvcDBEntities();
+            User user = new User
+            {
+                Name = "u4test"
+            };
+            dbCxt.Set<User>().Add(user);
+            dbCxt.SaveChanges();
+
+            Console.WriteLine(dbCxt.Set<User>().ToList().Count);
+            Console.ReadLine();
+        }
+    }
+}
