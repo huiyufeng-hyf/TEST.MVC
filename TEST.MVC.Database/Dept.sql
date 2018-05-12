d8a76c6bc34aef5700c424898de1041be302b1af
Michael <Michael@HYF>
2018-05-12 13:11:06 +0800

    Added database project initially.

---
+++ TEST.MVC.Database/Dept.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Dept]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(50) NOT NULL, 
+    [Created] DATETIME NULL, 
+    [Author] INT NULL, 
+    [Modified] DATETIME NULL, 
+    [Editor] INT NULL, 
+    [IsDeleted] BIT NULL
+)
---
+++ TEST.MVC.Database/UserLog.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[UserLog]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Operation] NVARCHAR(50) NOT NULL, 
+    [Created] DATETIME NULL, 
+    [IP] NVARCHAR(50) NULL, 
+    [Browser] NVARCHAR(50) NULL, 
+    [UserId] INT NOT NULL, 
+    CONSTRAINT [FK_UserLog_User] FOREIGN KEY ([UserId]) REFERENCES [User]([Id])
+)
