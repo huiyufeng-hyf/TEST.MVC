e90b1c614468d9fe709fa2c97849e2c78f54b63c
Michael <Michael@HYF>
2018-05-12 12:21:09 +0800

    added test web project initially.

---
+++ TEST.MVC/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TEST.MVC.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        [ValidateInput(false)]
+        public ActionResult Index(FormCollection fc)
+        {
+            var content = fc["editor"];
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
